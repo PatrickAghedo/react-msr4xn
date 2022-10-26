7aeffdfa653ba8edfa12b383fcbd44bddb6bd8b2
PatrickAghedo <aghedopatrick@gmail.com>
2022-10-26 22:53:38 +0100

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Components } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Users from './components/Users';
-import './style.css';
+import React, { Components } from "react";
+import { Routes, Route } from "react-router-dom";
+import PageNotFound from "./components/404page";
+import Home from "./components/Home";
+import Users from "./components/Users";
+import "./style.css";
 
 function App() {
   return (
     <div className="app">
       <Routes>
+        <Route path="*" element={<PageNotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
       </Routes>
